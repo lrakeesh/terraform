df5b6600f048d04a7e90ba2e797968f71146ae9a
lrakeesh <Rockmanju@yahoo.com>
2026-03-08 14:07:59 +0530

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "eu-west-1"
+  region = "ap-south-1"
 }
 
 data "aws_ami" "ubuntu" {
@@ -26,19 +26,19 @@ locals {
   instances = {
     instance1 = {
       ami           = data.aws_ami.ubuntu.id
-      instance_type = "t2.micro"
+      instance_type = "t3.micro"
     }
     instance2 = {
       ami           = data.aws_ami.ubuntu.id
-      instance_type = "t2.micro"
+      instance_type = "t3.micro"
     }
     instance3 = {
       ami           = data.aws_ami.ubuntu.id
-      instance_type = "t2.micro"
+      instance_type = "t3.micro"
     }
     instance4 = {
       ami           = data.aws_ami.ubuntu.id
-      instance_type = "t2.micro"
+      instance_type = "t3.micro"
     }
   }
 }
